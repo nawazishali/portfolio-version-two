c6dd71144cdfa95474c69b9619da0a2849a545db
Nawazish Ali <nawazishaliejaz@gmail.com>
2016-10-01 03:14:12 +0000

    Update main.js

--- js/main.js
+++ js/main.js
@@ -10,7 +10,7 @@ $(document).ready(function () {
                 if (target.length) {
                     $('html, body').animate({
                         scrollTop: target.offset().top
-                    }, 1000, 'swing');
+                    }, 600, 'swing');
                     return false;
                 }
             }
